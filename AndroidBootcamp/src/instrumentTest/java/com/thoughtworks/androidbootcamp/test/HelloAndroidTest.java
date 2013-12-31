5d137c44445bdcc97bb02a53246a3fe6f44a1915
Mary-Anne Cosgrove <mcosgro@thoughtworks.com>
2013-12-31 17:22:41 +1100

    Add the first Espresso Test

--- AndroidBootcamp/src/instrumentTest/java/com/thoughtworks/androidbootcamp/test/HelloAndroidTest.java
+++ AndroidBootcamp/src/instrumentTest/java/com/thoughtworks/androidbootcamp/test/HelloAndroidTest.java
@@ -1,30 +1,40 @@
 package com.thoughtworks.androidbootcamp.test;
 
-import android.annotation.TargetApi;
-import android.os.Build;
 import android.test.ActivityInstrumentationTestCase2;
 
+import com.thoughtworks.androidbootcamp.R;
 import com.thoughtworks.androidbootcamp.controller.HelloAndroid;
+
+import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.click;
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.typeText;
+import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.containsString;
 
 /**
  * Functional test for the HelloAndroid activity
  */
 public class HelloAndroidTest extends ActivityInstrumentationTestCase2<HelloAndroid> {
 
-    private HelloAndroid mActivity;
-
-    @TargetApi(Build.VERSION_CODES.FROYO)
+    @SuppressWarnings("deprecation")
     public HelloAndroidTest() {
-        super(HelloAndroid.class);
+        // This constructor was deprecated - but we want to support lower API levels.
+        super("com.thoughtworks.androidbootcamp", HelloAndroid.class);
     }
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-        mActivity = getActivity();
+        // Espresso will not launch our activity for us, we must launch it via getActivity().
+        getActivity();
     }
 
-    public void testSomething() {
-        fail();
+    public void testRetrievePlayerName() {
+        onView(withId(R.id.player_field)).perform(typeText("Fred"));
+        onView(withId(R.id.player_ok_button)).perform(click());
+
+        onView(withId(R.id.welcome_player)).check(matches(withText(containsString("Fred"))));
     }
 }
