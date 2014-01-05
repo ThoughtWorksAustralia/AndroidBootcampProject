5bb1804d8b1968489ffbb9d98326085f7c862872
Mary-Anne Cosgrove <mcosgro@thoughtworks.com>
2014-01-05 18:06:11 +1100

    Add reference to Parcelable vs Serializable article

--- AndroidBootcamp/src/main/java/com/thoughtworks/androidbootcamp/model/Game.java
+++ AndroidBootcamp/src/main/java/com/thoughtworks/androidbootcamp/model/Game.java
@@ -3,6 +3,8 @@ package com.thoughtworks.androidbootcamp.model;
 import java.io.Serializable;
 
 public class Game implements Serializable {
+    //For best performance, implement Parcelable rather than Serializable
+    //See http://www.developerphil.com/parcelable-vs-serializable/, for example
     private String player;
 
     public Game(String player) {
