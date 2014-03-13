205897d08c958d37e42f908bc1634dcc319a0c08
Alex Metcalfe <metcalfe.alex@gmail.com>
2014-03-13 22:37:03 +1100

    Create an Intent with an action to invoke the camera app Store the treasures photo path and the tobe taken photos path in fields Store the grid list adapter in a field so that we can use it later

--- AndroidBootcamp/src/main/java/com/thoughtworks/androidbootcamp/controller/fragment/TreasureListFragment.java
+++ AndroidBootcamp/src/main/java/com/thoughtworks/androidbootcamp/controller/fragment/TreasureListFragment.java
@@ -3,7 +3,12 @@ package com.thoughtworks.androidbootcamp.controller.fragment;
 
 
 import android.app.Fragment;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +17,11 @@ import android.widget.GridView;
 
 import com.thoughtworks.androidbootcamp.R;
 import com.thoughtworks.androidbootcamp.controller.adapter.TreasureListAdapter;
+import com.thoughtworks.androidbootcamp.util.FileUtils;
 import com.thoughtworks.androidbootcamp.util.TreasureLoader;
+
+import java.io.File;
+import java.io.IOException;
 
 
 /**
@@ -21,7 +30,15 @@ import com.thoughtworks.androidbootcamp.util.TreasureLoader;
  */
 public class TreasureListFragment extends Fragment {
 
+    private static final String TAG = "TreasureListFragment";
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+
     TreasureLoader treasureLoader;
+
+    private String mCurrentPhotoPath;
+    private String mSelectedTreasurePath;
+
+    private TreasureListAdapter mTreasureListAdapter;
 
     public TreasureListFragment() {
         // Required empty public constructor
@@ -45,14 +62,41 @@ public class TreasureListFragment extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         GridView gridView = (GridView) view.findViewById(R.id.treasure_list);
-        gridView.setAdapter(new TreasureListAdapter(getActivity(), treasureLoader.getSampleImagePaths()));
+        mTreasureListAdapter = new TreasureListAdapter(getActivity(), treasureLoader.getSampleImagePaths());
+        gridView.setAdapter(mTreasureListAdapter);
 
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+                mSelectedTreasurePath = (String) mTreasureListAdapter.getItem(position);
+                takePhoto();
             }
         });
 
+    }
+
+    public void takePhoto() {
+        // Create a new intent for taking a photo
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // Create the File where the photo should go
+        try {
+            File photoFile = FileUtils.getExternalPublicFile(Environment.DIRECTORY_PICTURES,
+                    getString(R.string.app_name));
+
+            // store the path so we know where it is later
+            mCurrentPhotoPath = "file:" + photoFile.getAbsolutePath();
+
+            // tell camera app when to put the photo
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+
+            if(intent.resolveActivity(getActivity().getPackageManager()) != null) {
+                // ask to use the camera
+                startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+            }
+
+        } catch (IOException ex) {
+            Log.e(TAG, "Error opening file", ex);
+        }
     }
 }
